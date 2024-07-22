911c3d4b9eb4deca5de78965b77664354a85c124
Florian Schaller <florian.schaller@stemys.io>
2024-07-22 09:02:48 +0200

    Generate fancy invoice PDF using Handlebars

--- apps/pdf-generator-server-e2e/src/pdf-generator-server/pdf-generator-server.spec.ts
+++ apps/pdf-generator-server-e2e/src/pdf-generator-server/pdf-generator-server.spec.ts
@@ -1,40 +1,53 @@
 import axios, { AxiosError } from 'axios';
 import { PDFDocumentData } from '@stemys/pdf-generator/constants';
 import * as path from 'path';
-import { outputFileSync } from 'fs-extra';
+import { outputFileSync, readFileSync } from 'fs-extra';
+import * as invoiceData from '../assets/templates/invoice.hbs.json';
 
 describe('POST /api/pdf-generator/generate', () => {
+  const distFolderPath = path.resolve(
+    __dirname,
+    '../../../../dist/apps/pdf-generator-server-e2e'
+  );
+  const templatesFolderPath = path.resolve(__dirname, '../assets/templates');
+  const invoiceTemplate = readFileSync(
+    path.join(templatesFolderPath, 'invoice.hbs'),
+    'utf-8'
+  );
+
   it('generate a PDF with the data passed in the request body', async () => {
     const data: PDFDocumentData = {
-      template: `<h1>{{title}}</h1>
-      <p>{{body}}</p>
-      <p>{{footer}}</p>`,
-      data: {
-        title: 'This is the title',
-        body: 'This is the body',
-        footer: 'This is the footer'
-      },
+      template: invoiceTemplate,
+      data: invoiceData,
       metadata: {
         title: 'Test title',
         subject: 'Test subject',
         author: 'This is the author',
         producer: 'This is the producer',
-        creator: 'This is the creator'
+        creator: 'This is the creator',
       },
-    }
+    };
     try {
       const res = await axios.post(`/api/pdf-generator/generate`, data);
-      const distFolderPath = path.resolve(__dirname, '../../../../dist/apps/pdf-generator-server-e2e'); // Adjust the path to your dist folder
-      const outputPath = path.join(distFolderPath, 'test.pdf');
-      const buffer = Buffer.from(res.data.data);
-      outputFileSync(outputPath, buffer);
+      outputFile('invoice.pdf', res.data.data);
       expect(res.status).toBe(201);
     } catch (error) {
-      if (error instanceof AxiosError) {
-        console.log(error.response?.data);
+      if (error instanceof AxiosError && error.response) {
+        throw new Error(`Request failed with status ${error.response?.status}: ${error.response?.data}`);
       } else {
-        console.log(error);
+        throw new Error(`An unexpected error occurred : ${error.message}`);
       }
     }
   });
+
+  const outputFile = (fileName: string, file: ArrayBuffer) => {
+    const outputPath = path.join(distFolderPath, fileName);
+    const buffer = Buffer.from(file);
+    try {
+      outputFileSync(outputPath, buffer);
+      console.log(`File saved at ${outputPath}`);
+    } catch {
+      throw new Error(`Failed to save the file at ${outputPath}`);
+    }
+  };
 });
